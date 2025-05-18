2cff9bc8007b3b93c491c767938922055ddd74cb
deepak belamana <saideepak627@gmail.com>
2025-05-18 07:36:08 +0530

    18/05     - Feign Client & Circuit Breaker Fallback method added     - the fallback back method should have the same return type like the orginal method and also the parameters,     - additionally it should have one more parameter of type throwable     - the method should be in the same class/interface of the feign client

--- src/main/java/com/dpkprojects/app/photoapp/api/users/data/clients/AlbumClientService.java
+++ src/main/java/com/dpkprojects/app/photoapp/api/users/data/clients/AlbumClientService.java
@@ -1,14 +1,23 @@
 package com.dpkprojects.app.photoapp.api.users.data.clients;
 
 import com.dpkprojects.app.photoapp.api.users.ui.model.album.AlbumResponseModel;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @FeignClient(name = "album-service")
 public interface AlbumClientService {
-    @GetMapping("/users/{userId}/albumss")
+    @GetMapping("/users/{userId}/albums")
+    @CircuitBreaker(name = "album-service", fallbackMethod = "getAlbumsFallbackMethod")
     public List<AlbumResponseModel> getAlbums(@PathVariable String userId);
+
+    default List<AlbumResponseModel> getAlbumsFallbackMethod(String userId, Throwable exception) {
+        System.out.println("user id" + userId);
+        System.out.println("exception took place :" + exception.getMessage());
+        return new ArrayList<>();
+    }
 }
